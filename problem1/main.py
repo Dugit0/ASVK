2fc925a2e22754489407ccbe1f3ff32ebab4027d
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-03-27 21:33:42 +0300

    Add commentari in main.py

--- problem1/main.py
+++ problem1/main.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
 import random
+import sys
 
-# FILE = "input.xml"
-FILE = "test_out.xml"
-MAX_STEPS = 5000
+FILE = "input.xml" # Путь до файла с входными данными
+MAX_STEPS = 5000   # Максимальное число итераций алгоритма
 
 
 def find_load(solution, links):
+    # Функция для поиска суммарной нагрузки на сеть для решения
+    # solution - найденное решение
+    # links - список программ, которые обмениваются данными
+
     ans = 0
     for link in links:
         first_prog = link[0]
@@ -17,6 +21,11 @@ def find_load(solution, links):
 
 
 def cheсk(solution, proc_limits, prog_cap):
+    # Функция для проверки корректности найденного решения
+    # solution - найденное решение
+    # proc_limits - список с предельными нагрузками на процессоры
+    # prog_cap - список с нагрузками на процессор для каждой программы
+    
     proc_num = len(proc_limits)
     prog_num = len(prog_cap)
     cur_proc_limits = [0] * proc_num
@@ -28,45 +37,86 @@ def cheсk(solution, proc_limits, prog_cap):
     return True
 
 
+# Открытие и считывание файла
 with open(FILE, encoding="utf-8") as f_inp:
-    soup = BeautifulSoup(f_inp.read(), "lxml")
+    try:
+        soup = BeautifulSoup(f_inp.read(), "lxml")
+    except:
+        print("Неверный формат .xml файла", file=sys.stderr)
+        sys.exit(1)
 
+# Парсинг файла с входными данными с проверкой на корректность формата файла
 data = soup.data
-# proc_num = int(data.processors_num.text)
+if data == None:
+    print("Не найден тег data", file=sys.stderr)
+    sys.exit(1)
+
+if data.processors == None:
+    print("Не найден тег processors", file=sys.stderr)
+    sys.exit(1)
+# Список с предельными нагрузками на процессоры
 proc_limits = [int(tag.text) for tag in data.processors.find_all("limit")]
+
+# Число процессоров
 proc_num = len(proc_limits)
-# prog_num = int(data.programs_num.text)
+if proc_num == 0:
+    print("Недопустимое число процессоров (0 процессоров)", file=sys.stderr)
+    sys.exit(1)
+
+if data.programs == None:
+    print("Не найден тег programs", file=sys.stderr)
+    sys.exit(1)
+# Список с нагрузками на процессор для каждой программы
 prog_cap = [int(tag.text) for tag in data.programs.find_all("capacity")]
+
+# Число программ
 prog_num = len(prog_cap)
-links = [(int(tag.first.text), int(tag.second.text), int(tag.load.text)) for tag in data.networks.find_all("netlink")]
-# links.sort()
+if prog_num == 0:
+    print("Недопустимое число программ (0 программ)", file=sys.stderr)
+    sys.exit(1)
 
-print("proc_limits :", proc_limits)
-print("prog_cap :", prog_cap)
-# print("links :", links)
+try:
+    # unsorted_links - список программ, которые обмениваются данными в формате:
+    # (первая_программа, вторая_программа, нагрузка_на_сеть)
+    unsorted_links = [(int(tag.first.text), int(tag.second.text), int(tag.load.text)) for tag in
+                      data.networks.find_all("netlink")]
+except:
+    print("Неверный формат тега netlink", file=sys.stderr)
+    sys.exit(1)
 
-best_load = sum(map(lambda a: a[2], links))
+# links - отсортированный список программ, которые обмениваются данными в формате:
+links = []
+for link in unsorted_links:
+    min_prog = min(link[0], link[1])
+    max_prog = max(link[0], link[1])
+    links.append((min_prog, max_prog, link[2]))
+links.sort(key=lambda a: a[0])
+
+# Расчет теоретически максимальной нагрузки
+best_load = sum(map(lambda a: a[2], links)) + 1
 best_solution = [-1] * prog_num
-# print(f"best_load = {best_load}")
 
-global_count_steps = 0
-count_steps = 0
-flag_success = False
+global_count_steps = 0   # Глобальный счетчик итераций
+count_steps = 0          # Счетчик для подсчета итеграций между двумя найденными решениями
+flag_success = False     # Флаг нахождения решения
 while True:
     count_steps += 1
     global_count_steps += 1
+    # Генерируем решение
     new_solution = [random.randint(0, proc_num - 1) for i in range(prog_num)]
+    # Считаем нагрузку
     new_load = find_load(new_solution, links)
+    # Проверяем что найденное решение лучше известного
     if new_load < best_load and cheсk(new_solution, proc_limits, prog_cap):
         flag_success = True
         best_load = new_load
         best_solution = new_solution.copy()
         count_steps = 0
+    # Условие выхода из цикла
     if best_load == 0 or count_steps >= MAX_STEPS:
         break
 
-# print("cont_steps =", count_steps)
-
+# Вывод результата
 if flag_success:
     print("success")
     print(global_count_steps)
